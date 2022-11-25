8ea7c1cc18a0d2233e8c4f228a3fa2c0c836a89b
sanilsharma <sanilsharma786@gmail.com>
2022-11-25 14:43:28 +0530

    setup

---
+++ src/main/java/com/tool/websitemonitoringtool/WebsiteMonitoringToolApplication.java
@@ -0,0 +1,13 @@
+package com.tool.websitemonitoringtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteMonitoringToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteMonitoringToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tool/websitemonitoringtool/WebsiteMonitoringToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tool.websitemonitoringtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteMonitoringToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
